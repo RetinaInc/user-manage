715a3273e1d5952f2539a17304c5b9ea832d9563
YigWoo <wooyichao@gmail.com>
2013-12-30 21:48:54 +0800

    commit before push to github

--- src/test/java/com/yigwoo/simple/web/LoginControllerTests.java
+++ src/test/java/com/yigwoo/simple/web/LoginControllerTests.java
@@ -13,35 +13,20 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(locations = { "classpath:applicationContext.xml",
-		"file:src/main/webapp/WEB-INF/spring-mvc.xml" })
-public class LoginControllerTests extends AbstractShiroTest {
+public class LoginControllerTests extends AbstractContextControllerTests {
 
 	@Autowired
 	private WebApplicationContext wac;
 	private MockMvc mockMvc;
 	private Logger logger = LoggerFactory.getLogger(LoginControllerTests.class);
-
-	@BeforeClass
-	public static void setup() {
-		Factory<SecurityManager> factory = new WebIniSecurityManagerFactory(
-				Ini.fromResourcePath("classpath:shiro.ini"));
-		setSecurityManager(factory.getInstance());
-	}
 
 	@Before
 	public void beforeTests() {
@@ -61,10 +46,6 @@ public class LoginControllerTests extends AbstractShiroTest {
 	 */
 	@Test
 	public void submitSuccess() throws Exception {
-		Subject subjectUnderTest = new Subject.Builder(getSecurityManager())
-				.buildSubject();
-		setSubject(subjectUnderTest);
-		
 		mockMvc.perform(
 				post("/login")
 				.param("username", "superuser")
@@ -72,8 +53,4 @@ public class LoginControllerTests extends AbstractShiroTest {
 				.andDo(print());
 	}
 
-	@After
-	public void tearDownSubject() {
-		clearSubject();
-	}
 }
--- src/test/java/com/yigwoo/simple/web/ProfileControllerTests.java
+++ src/test/java/com/yigwoo/simple/web/ProfileControllerTests.java
@@ -1,5 +1,57 @@
 package com.yigwoo.simple.web;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.web.config.WebIniSecurityManagerFactory;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
 public class ProfileControllerTests extends AbstractContextControllerTests {
 
+	@Autowired
+	protected WebApplicationContext wac;
+	private MockMvc mockMvc;
+	
+	@Before
+	public void beforeTest() {
+		SecurityManager securityManager = (DefaultWebSecurityManager) wac.getBean("securityManager");
+		setSecurityManager(securityManager);
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void getForm() throws Exception {
+		Subject subject = new Subject.Builder(getSecurityManager()).buildSubject();
+		AuthenticationToken token = new UsernamePasswordToken("superuser", "superuser"); 
+		subject.login(token);
+		setSubject(subject);
+		this.mockMvc.perform(
+				get("/profile")
+				.accept(MediaType.TEXT_HTML)
+				)
+				.andDo(print());
+	}
+
+	@After
+	public void afterTest() {
+		clearSubject();
+	}
+	
 }
